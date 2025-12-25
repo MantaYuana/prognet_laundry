aae2f22aab557af779971cee59a2522795871e53
Dash <daneshaurus@gmail.com>
2025-12-25 11:05:35 +0800

    Fix: Outlet ketinggalan

--- app/Models/Outlet.php
+++ app/Models/Outlet.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\User;
 
 class Outlet extends Model {
     use HasFactory, SoftDeletes;
@@ -13,7 +14,6 @@ class Outlet extends Model {
         'name',
         'address',
         'phone_number',
-        'user_id'
     ];
 
     public function owner() {
@@ -24,7 +24,12 @@ class Outlet extends Model {
         return $this->hasMany(Staff::class);
     }
 
-    public function services() {
+    public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+    public function services(){
         return $this->hasMany(LaundryService::class);
     }
 }
